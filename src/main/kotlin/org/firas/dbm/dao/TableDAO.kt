e90c32591b22be097bb69f4a7ad2c347cf968197
Firas <448445223@qq.com>
2018-10-15 21:40:57 +0800

    Add some DAO

---
+++ src/main/kotlin/org/firas/dbm/dao/TableDAO.kt
@@ -0,0 +1,20 @@
+package org.firas.dbm.dao
+
+import org.firas.common.dao.DaoBase
+import org.firas.dbm.po.TablePO
+import org.springframework.stereotype.Repository
+
+/**
+ * <b><code></code></b>
+ * <p/>
+ *
+ * <p/>
+ *
+ * <b>Creation Time:</b> 2018年10月11日
+ *
+ * @author Wu Yuping
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Repository
+interface TableDAO: DaoBase<TablePO, String>
